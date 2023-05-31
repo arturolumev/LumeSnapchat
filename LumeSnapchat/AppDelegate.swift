e230aa11633287839c873b1aca211e6143771075
Edwin Arturo Lume Ventura <edwin.lume@tecsup.edu.pe>
2023-05-31 15:49:51 -0500

    inicio de sesion con google

--- LumeSnapchat/AppDelegate.swift
+++ LumeSnapchat/AppDelegate.swift
@@ -7,15 +7,19 @@
 
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+//        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
+        
+        
         return true
     }
 
@@ -28,9 +32,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
 
